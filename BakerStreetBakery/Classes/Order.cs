f19138f309f0d8eeebd425369d3470e1e994a4b4
jacobbrown4 <jacobbrown4@gmail.com>
2022-03-10 10:31:10 -0500

    Started baker street

---
+++ BakerStreetBakery/Classes/Order.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BakerStreetBakery.Classes
+{
+    public class Order
+    {
+        public Order(Product product)
+        {
+            Product = product;
+            Batches = 1;
+        }
+        public Order(Product product, int batches) : this(product)
+        {
+            Batches = batches;
+        }
+        public Order(Product product, int batches,int orderNumber):this(product, batches)
+        {
+            OrderNumber = orderNumber;
+        }
+        public Order(Product product, int batches,int orderNumber, string customer) : this(product, batches, orderNumber)
+        {
+            CustomerName = customer;
+        }
+        public Product Product { get; set; }
+        public int Batches { get; set; }
+        public int OrderNumber { get; set; }
+        public string CustomerName { get; set; }
+        public decimal TotalCost //Readonly
+        {
+            get
+            {
+                decimal initialCost = Product.Price * Batches;
+                decimal upcharge = initialCost + 100;
+                return upcharge;
+            }
+        }
+    }
+}
---
+++ BakerStreetBakery/Classes/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BakerStreetBakery
+{
+    public abstract class Product
+    {
+        public virtual decimal Price => 1m;
+    }
+    public class Bread : Product
+    {
+        public override decimal Price => 500.01m;
+    }
+    public class Cake : Product
+    {
+        public override decimal Price => 2000m;
+    }
+    public class Pastry : Product
+    {
+        public override decimal Price => 200.1m;
+    }
+    public class Pies : Product
+    {
+        public override decimal Price => 851.5m;
+    }
+    public class Donut : Product
+    {
+        public override decimal Price => 5000m;
+    }
+}
